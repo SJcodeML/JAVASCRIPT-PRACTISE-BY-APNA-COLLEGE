ed42c0add659d3babe5ade8a3772b08649c0aebd
SJcodeML <sidrajabin56@gmail.com>
2024-11-10 03:11:30 +0500

    code added

---
+++ javascript10/script.js
@@ -0,0 +1,17 @@
+// classList Property 
+ 
+let heading = document.querySelector("h1");
+heading.classList.add("green");
+
+let divv =document.querySelector("div");
+divv.classList.remove("box");
+
+
+divv.classList.add("box");
+
+divv.classList.contains("box");
+
+divv.classList.toggle("box");
+
+
+
